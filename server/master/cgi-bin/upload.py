6d0346aeb4286fcb3611344b23ef866b9a66e585
Karthik Kumar <kkumar91@vt.edu>
2012-11-23 20:59:25 -0500

    added comments. also displays how long computation took now

--- server/master/cgi-bin/upload.py
+++ server/master/cgi-bin/upload.py
@@ -8,6 +8,7 @@ import time
 import cgi
 import cgitb; cgitb.enable()  # for troubleshooting
 import subprocess;
+#This is the top of the html page
 print "Content-type: text/html"
 print
 
@@ -20,33 +21,50 @@ print """
 
   <h3> Distributed OCR Results Page </h3>
 """
-
-timestamp = int(time.time())
+#The time when the request was received
+start = int(time.time())
+#used to get the file
 form = cgi.FieldStorage()
+#look for datafile in the html page
 filedata = form['datafile']
+#make sure there is a file to upload 
 if filedata.filename:
-	fn = str(timestamp) + os.path.basename(filedata.filename)
+	#create the file using the start
+	fn = str(start) + os.path.basename(filedata.filename)
+	#write the image in the ../uploads dir.
 	open('../uploads/' + fn, 'wb').write(filedata.file.read())
+	#make sure the file is a png image
 	if imghdr.what('/var/www/uploads/'+fn) is not 'png':
 		print "Please go back and upload a png image"
 		sys.exit(1)	
 else: 
 	print "Please go back and upload an image"
 	sys.exit(1)
-#args = ['java', '-jar', 'Split.jar', '/var/www/uploads/' + filedata.filename, '/home/ec2-user/images_to_send/']
-os.makedirs('/var/www/uploads/images_to_send/'+str(timestamp)+'/')
-args = ['java', '-jar', 'Split.jar', '/var/www/uploads/' + str(timestamp) + filedata.filename, '/var/www/uploads/images_to_send/'+str(timestamp)+'/']
-#print args
+
+###############################################################
+computationstart = int(time.time())
+#Create a directory for this image's split sections
+os.makedirs('/var/www/uploads/images_to_send/'+str(start)+'/')
+#run the split program, which runs the IDT jar and saves each split in the above dir
+args = ['java', '-jar', 'Split.jar', '/var/www/uploads/' + str(start) + filedata.filename, '/var/www/uploads/images_to_send/'+str(start)+'/']
 subprocess.call(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 #proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 #print proc.communicate()
 
-args = ['java', '-jar', 'SendCombine.jar', '/var/www/uploads/images_to_send/' + str(timestamp)+'/']
+#next, run the send andcombine jar. it sends the split images and the combines the text we get back
+args = ['java', '-jar', 'SendCombine.jar', '/var/www/uploads/images_to_send/' + str(start)+'/']
 #print args
 proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 message = str(proc.communicate())
+computationend = int(time.time())
+###############################################################
+
+totaltime = computationend - computationstart
+
+#replace the \n in the recieved message with line breaks, to show in the html page
 message = message.replace("\\n", "<br />")
-image = '/uploads/'+str(timestamp)+filedata.filename+"\""
+#html code to show image that was uploaded and the result of the computation
+image = '/uploads/'+str(start)+filedata.filename+"\""
 print """
 <h5>Input Image:</h5>
 <br/>
@@ -57,10 +75,14 @@ print """
 </br>
 </body>
  Image Contents:%s 
-</html>
 """ %message
-
-folder = '/var/www/uploads/images_to_send/' + str(timestamp)+'/'
+print """
+</br>
+ Total time (seconds) required for computation: %d
+</html>
+""" %totaltime
+#delete all the images, excpet the originial (need to display)
+folder = '/var/www/uploads/images_to_send/' + str(start)+'/'
 try:
 	for file in os.listdir(folder):
 		file_path = os.path.join(folder, file)
@@ -69,8 +91,8 @@ try:
 		else:
 			shutil.rmtree(file_path)
 	os.removedirs(folder)
-	#os.remove('/var/www/uploads/'+str(timestamp)+filedata.filename)
-	textfile = string.replace(str(timestamp)+filedata.filename, ".png", "_output.txt")
+	#os.remove('/var/www/uploads/'+str(start)+filedata.filename)
+	textfile = string.replace(str(start)+filedata.filename, ".png", "_output.txt")
 	os.remove('/var/www/uploads/'+textfile)
 except:
 	print "Unexpected Error"
